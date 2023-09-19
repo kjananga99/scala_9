4ebffb40aed2a331fbfc13e245f14409ca8754da
Jananga Wijewardhana <136483747+kjananga99@users.noreply.github.com>
2023-09-19 08:24:32 +0530

    Add scala files

---
+++ Q2.scala
@@ -0,0 +1,39 @@
+object Q2 {
+
+  class Rational(n: Int, d: Int) {
+    require(d != 0, "Denominator cannot be zero")
+
+    private val gcdValue: Int = gcd(n, d)
+    val numer: Int = n / gcdValue
+    val denom: Int = d / gcdValue
+
+    override def toString: String = {
+      if (denom < 0) {
+        s"-${numer.abs}/${denom.abs}"
+      } else {
+        s"$numer/${denom.abs}"
+      }
+    }
+
+    def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+    def neg: Rational = new Rational(-numer, denom)
+    def sub(other: Rational): Rational = {
+      new Rational(numer * other.denom - other.numer * denom, denom * other.denom)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val x = new Rational(3, 4)
+    val y = new Rational(5, 7)
+    val z = new Rational(1, 8)
+
+    val result = x.sub(y).sub(z)
+
+    println(s"x = $x")
+    println(s"y = $y")
+    println(s"z = $z")
+    println(s"x - y - z = $result")
+}
+
+}
